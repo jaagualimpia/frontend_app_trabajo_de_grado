572969c6fb126134098767be96be45652730881f
jaagualimpia <jaagualimpia25@gmail.com>
2024-04-09 20:11:05 -0500

    Images of classes added in home screen

---
+++ src/app/components/listItemWithImage/ListItemWithImage.tsx
@@ -0,0 +1,22 @@
+import { Row, Col } from "react-bootstrap"
+
+interface params {
+    label: string
+    imageSrc: string
+    alt: string
+}
+
+export const ListItemWithImage = ({ label, imageSrc , alt}: params) => {
+    return (
+        <li className="my-3">
+            <Row>
+                <Col className="fw-bold my-2">
+                    {label}
+                </Col>
+                <Row className="justify-content-center">
+                    <img src={imageSrc} alt="Imagen de carcinoma de celulas grandes" style={{height: "250px", width: "250px"}}/>
+                </Row>
+            </Row>
+        </li>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 import NavBar from "./components/navbar/NavBar";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { ListItemWithImage } from "./components/listItemWithImage/ListItemWithImage";
 
 export default function Home() {
   const router = useRouter()
@@ -29,10 +30,34 @@ export default function Home() {
 
                   <Row className="mx-3">
                     <Col>
-                      <p className="lh-lg">Este es un trabajo realizado por estudiantes de la universidad San Buenaventura de Cali, el propósito del presente proyecto es el de apoyar el diagnostico del cáncer de pulmón.</p>
+                      <p className="lh-md">Este es un trabajo realizado por estudiantes de la universidad San Buenaventura de Cali. <br />
+                        En la herramienta desarrollado se construyo un sistema de inteligencia artificial que permite clasificar distintos tipos de cancer de púlmon junto con pulmones sanos. Las clases que se clasifica son:
+                      </p>
+                      <ul>
+                        <Container>
+                          <Row>
+                            <Col>
+                              <ListItemWithImage label="Pulmones sanos" imageSrc="/images/normal_lung_image.jpg" alt="Imagen de púlmones normales" />
+                            </Col>
+                            <Col>
+                              <ListItemWithImage label="Carcinoma de celulas grandes" imageSrc="/images/large_cell_carcinoma.png" alt="Imagen de carcinoma de celulas grandes" />
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col>
+                              <ListItemWithImage label="Carcinoma de celulas escamosas" imageSrc="/images/squamous_cell_carcinoma.png" alt="Imagen de carcinoma de celulas escamosas" />
+                            </Col>
+                            <Col>
+                              <ListItemWithImage label="Adenocarcinoma" imageSrc="/images/Adenocarcinoma.png" alt="Imagen de adenocarcinoma" />
+                            </Col>
+                          </Row>
+                        </Container>
+                      </ul>
+                      <p className="lh-md">
+                        El modelo fue entrenado  
+                      </p>
                     </Col>
                   </Row>
-
                   <Row>
                     <Col>
                       <p className="text-center fs-1 mb-4 fw-bold">Recursos</p>
@@ -46,13 +71,13 @@ export default function Home() {
                     </Col>
 
                     <Col style={{ cursor: "pointer" }}>
-                      <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="github image" style={{ height: "100px" }} onClick={() => router.push("https://github.com/jaagualimpia/backend_app_trabajo_de_grado/tree/joragucai")}/>
+                      <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="github image" style={{ height: "100px" }} onClick={() => router.push("https://github.com/jaagualimpia/backend_app_trabajo_de_grado/tree/joragucai")} />
                       <p className="fw-bold fs-5">Repositorio backend app</p>
                     </Col>
 
                     <Col style={{ cursor: "pointer" }}>
                       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Colaboratory_SVG_Logo.svg/2560px-Google_Colaboratory_SVG_Logo.svg.png" alt="github image" style={{ height: "100px" }}
-                           onClick={() => router.push("https://www.kaggle.com/code/jorgeagualimpia/notebook-del-trabajo-de-grado-final ")}
+                        onClick={() => router.push("https://www.kaggle.com/code/jorgeagualimpia/notebook-del-trabajo-de-grado-final ")}
                       />
                       <p className="fw-bold fs-5">Jupyter notebook del modelo de IA</p>
                     </Col>
